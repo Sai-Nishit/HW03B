4cf97cdda3edbec4c44440588e70972509227373
Sai Nishit Ragupathy <sr6931@nyu.edu>
2024-10-27 21:25:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,32 @@
+let lvLogo;
+let flower;
+
+function preload() {
+  lvLogo = loadImage('LV.jpg'); 
+  flower = loadImage('star.jpg'); }
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(102, 51, 0); 
+
+  let gridSize = 100; 
+
+  for (let x = 0; x < width; x += gridSize) {
+    for (let y = 0; y < height; y += gridSize) {
+      fill(102, 51, 0);
+      noStroke();
+      rect(x, y, gridSize, gridSize); 
+      if ((x + y) % 200 === 0) {
+        image(lvLogo, x, y, gridSize, gridSize); 
+      } else {
+        image(flower, x, y, gridSize, gridSize); 
+      }
+    }
+  }
 }
 
-function draw() {
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(102, 51, 0); 
+  redraw(); 
 }
